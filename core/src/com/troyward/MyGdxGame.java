057f9ef5c8f5d9de85061dff473ce5c95c61a14c
Troy <tward4@tulane.edu>
2016-09-30 13:48:37 -0400

    Trying to add animation

--- core/src/com/troyward/MyGdxGame.java
+++ core/src/com/troyward/MyGdxGame.java
@@ -5,16 +5,22 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	TextureRegion down,up,right,left;
-	float x,y,xv,yv;
+	TextureRegion down,up,right,left,stand;
+	float x,y,xv,yv,totalTime;
+	boolean faceRight = true;
+	Animation walkRight;
+	Animation walkUp;
+	Animation walkDown;
 
-	static final float MAX_VELOCITY = 500;
+	static final float VELOCITY = 200;
+	static final float SPACE_VELOCITY = 400;
 	static final float FRICTION = 0.9f;
 	static final int WIDTH = 16;
 	static final int HEIGHT = 16;
@@ -30,17 +36,38 @@ public class MyGdxGame extends ApplicationAdapter {
 		down = grid[6][0];
 		up = grid[6][1];
 		right = grid[6][3];
+		stand = grid[6][2];
 		left = new TextureRegion(right);
 		left.flip(true,false);
+		walkRight = new Animation(0.3f,grid[6][2],grid[6][3]);
 	}
 
 	@Override
 	public void render () {
+		totalTime += Gdx.graphics.getDeltaTime();
 		move();
+
+		TextureRegion person;
+		if (yv > 0) {
+			person = up;
+		}
+		else if (yv < 0) {
+			person = down;
+		}
+		else if (xv > 0) {
+			person = walkRight.getKeyFrame(totalTime);
+		}
+		else if (xv < 0) {
+			person = left;
+		} else {
+			person = stand;
+		}
+
+
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, x, y);
+		batch.draw(person, x, y, DRAW_WIDTH, DRAW_HEIGHT);
 		batch.end();
 	}
 	
@@ -52,18 +79,37 @@ public class MyGdxGame extends ApplicationAdapter {
 
 	public void move () {
 		if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-			yv = MAX_VELOCITY;
+			yv = VELOCITY;
 		}
 		else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-			yv = MAX_VELOCITY * -1;
+			yv = VELOCITY * -1;
 		}
 
 		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-			xv = MAX_VELOCITY;
+			xv = VELOCITY;
+			faceRight = true;
 		}
 
 		else if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-			xv = MAX_VELOCITY * -1;
+			xv = VELOCITY * -1;
+			faceRight = false;
+		}
+
+		if (Gdx.input.isKeyPressed(Input.Keys.UP) && Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+			yv = SPACE_VELOCITY;
+		}
+		else if (Gdx.input.isKeyPressed(Input.Keys.DOWN) && Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+			yv = SPACE_VELOCITY * -1;
+		}
+
+		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) && Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+			xv = SPACE_VELOCITY;
+			faceRight = true;
+		}
+
+		else if (Gdx.input.isKeyPressed(Input.Keys.LEFT) && Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+			xv = SPACE_VELOCITY * -1;
+			faceRight = false;
 		}
 
 		x += xv * Gdx.graphics.getDeltaTime();
