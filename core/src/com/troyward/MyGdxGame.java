7bff449ed4df0b50ff0fe5941bed76db113cec10
Troy <tward4@tulane.edu>
2016-09-30 19:41:38 -0400

    Re-using fields

--- core/src/com/troyward/MyGdxGame.java
+++ core/src/com/troyward/MyGdxGame.java
@@ -25,8 +25,6 @@ public class MyGdxGame extends ApplicationAdapter implements Disposable {
 	Animation walkDown;
 	Music music;
 
-
-
 	static final float VELOCITY = 200;
 	static final float SPACE_VELOCITY = 400;
 	static final float FRICTION = 0.9f;
@@ -35,10 +33,12 @@ public class MyGdxGame extends ApplicationAdapter implements Disposable {
 	static final float DRAW_WIDTH = WIDTH * 2.5f;
 	static final float DRAW_HEIGHT = HEIGHT * 2.5f;
 
-
 	@Override
 	public void create () {
+		//music
 		music = Gdx.audio.newMusic(Gdx.files.external("Downloads/FluteyMinicraftSong.mp3"));
+
+		//creating person and animation
 		batch = new SpriteBatch();
 		img = new Texture("tiles.png");
 		TextureRegion[][] grid = TextureRegion.split(img,WIDTH,HEIGHT);
@@ -48,15 +48,19 @@ public class MyGdxGame extends ApplicationAdapter implements Disposable {
 		stand = grid[6][2];
 		left = new TextureRegion(right);
 		left.flip(true,false);
-		walkRight = new Animation(0.3f,grid[6][2],grid[6][3]);
-		walkUp = new Animation(0.3f,grid[6][2],grid[6][1]);
-		walkDown = new Animation(0.3f,grid[6][2],grid[6][0]);
+		walkRight = new Animation(0.3f,stand,right);
+		walkUp = new Animation(0.3f,stand,up);
+		walkDown = new Animation(0.3f,stand,down);
 	}
 
 	@Override
 	public void render () {
+		//play music
 		music.play();
+		//setting total time
 		totalTime += Gdx.graphics.getDeltaTime();
+
+		//windowed and person flipping
 		move();
 
 		TextureRegion person;
@@ -90,9 +94,11 @@ public class MyGdxGame extends ApplicationAdapter implements Disposable {
 			person = stand;
 		}
 
-
+		//color of background
 		Gdx.gl.glClearColor(0.2f, 0.85f, 0.2f, 0.75f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		//rending person
 		batch.begin();
 		if (faceRight) {
 			batch.draw(person, x, y, DRAW_WIDTH, DRAW_HEIGHT);
